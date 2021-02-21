2c11635ba0faa81a31a6f761e2b72be82a7c6e26
Saif <alsaif839@gmail.com>
2021-02-21 23:17:15 +0600

    boolean expression added

---
+++ basics/boolean.dart
@@ -0,0 +1,10 @@
+import 'dart:ffi';
+
+void main(){
+  bool is_true = false; //decalration of boolean variables
+  if (is_true){
+    print("The statement is true");
+  }else{
+    print('The statement is not false');
+  }
+}
